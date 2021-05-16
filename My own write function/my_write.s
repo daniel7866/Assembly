d0e0dbb8c9816e00a71fb66babd9994b4a7d4cf4
Daniel Fogel <65624776+daniel7866@users.noreply.github.com>
2021-05-16 23:14:46 +0300

    Update my_write.s

--- My own write function/my_write.s
+++ My own write function/my_write.s
@@ -9,9 +9,9 @@
 .section .text
 my_write:
 
-	mov $0x4, %eax # use the write syscall
-	mov $0x1, %ebx # write to standard output
-	mov 4(%esp), %ecx
-	mov 8(%esp), %edx
-	int $0x80      #trap
+	mov $0x4, %eax		# use the write syscall
+	mov $0x1, %ebx		# write to standard output
+	mov 4(%esp), %ecx	# first function parameter	- the string
+	mov 8(%esp), %edx	# second function parameter	- length of string
+	int $0x80      		# trap
 	ret
